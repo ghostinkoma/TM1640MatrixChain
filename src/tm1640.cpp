039a69e34d5e5b64a0bdf5b3d73d8fde94e940d0
ghostinkoma <ghostinkoma@gmail.com>
2025-11-14 01:57:55 +0900

    tm1640.cpp を更新

--- src/tm1640.cpp
+++ src/tm1640.cpp
@@ -4,13 +4,13 @@
 TM1640::TM1640(uint8_t gpio_sclk, 
                uint8_t gpio_din, 
                int frequency_khz)
-              : _sclk_pin(gpio_sclk), 
-                _din_pin(gpio_din),
-                _frequency_khz(frequency_khz){
-  
-  _sclk_pin = gpio_sclk;
-  _din_pin  = gpio_din;
-  _frequency_khz = frequency_khz;
+  : _sclk_pin(gpio_sclk),
+    _din_pin(gpio_din),
+    _frequency_khz(frequency_khz){
+  // frequency_khz が 0 または負値ならデフォルト値（CPUクロック由来）を使用
+  if (_frequency_khz <= 0) {
+    _frequency_khz = (int)(TM1640_FCPU_HZ / 1000UL);  // Hz → kHz 換算
+  }
 
   pinMode(_sclk_pin, OUTPUT);
   pinMode(_din_pin, OUTPUT);
@@ -18,6 +18,7 @@ TM1640::TM1640(uint8_t gpio_sclk,
   digitalWrite(_sclk_pin, HIGH);
   digitalWrite(_din_pin, HIGH);
 }
+
 
 int TM1640::Display(bool on) {
   (void)on;
